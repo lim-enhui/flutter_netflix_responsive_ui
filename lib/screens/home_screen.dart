0b4214bbc56d3d8d11985bc67d7c2f7f3a4c80ba
lim-enhui <lim.enhui@outlook.com>
2022-02-09 04:43:38 +0800

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix_responsive_ui/cubits/app_bar/app_bar_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../data/data.dart';
+import '../widgets/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late ScrollController _scrollController;
+
+  // late double _scrollOffset = 0.0;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(() {
+        // setState(() {
+        //   _scrollOffset = _scrollController.offset;
+        // });
+        context.read<AppBarCubit>().setOffset(_scrollController.offset);
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.black,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey.shade800,
+        child: const Icon(Icons.cast),
+        onPressed: () => print('Cast'),
+      ),
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 50.0),
+        child: BlocBuilder<AppBarCubit, double>(
+          builder: (context, scrollOffset) {
+            return CustomAppBar(scrollOffset: scrollOffset);
+          },
+        ),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: const [
+          SliverToBoxAdapter(
+            child: ContentHeader(featuredContent: sintelContent),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.only(top: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: Previews(
+                key: PageStorageKey('previews'),
+                title: 'Previews',
+                contentList: previews,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              key: PageStorageKey('myList'),
+              title: 'My List',
+              contentList: myList,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              key: PageStorageKey('originals'),
+              title: 'Netflix Originals',
+              contentList: originals,
+              isOriginals: true,
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.only(bottom: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: ContentList(
+                key: PageStorageKey('trending'),
+                title: 'Trending',
+                contentList: trending,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix_responsive_ui/widgets/widgets.dart';
+
+import '../assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  final double scrollOffset;
+
+  const CustomAppBar({
+    Key? key,
+    this.scrollOffset = 0.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: !Responsive.isDesktop(context)
+          ? const EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 20.0,
+            )
+          : const EdgeInsets.only(
+              left: 20.0,
+              right: 20.0,
+              top: 10.0,
+            ),
+      color:
+          Colors.black.withOpacity((scrollOffset / 350).clamp(0, 1).toDouble()),
+      child: const Responsive(
+        mobile: _CustomAppBarMobile(),
+        desktop: _CustomAppBarDesktop(),
+      ),
+    );
+  }
+}
+
+class _CustomAppBarMobile extends StatelessWidget {
+  const _CustomAppBarMobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Row(
+        children: [
+          Image.asset(Assets.netflixLogo0),
+          const SizedBox(width: 12.0),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _AppBarButton(
+                  title: 'TV Shows',
+                  onTap: () => print('TV Shows'),
+                ),
+                _AppBarButton(
+                  title: 'Movies',
+                  onTap: () => print('Movies'),
+                ),
+                _AppBarButton(
+                  title: 'My List',
+                  onTap: () => print('My List'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBarDesktop extends StatelessWidget {
+  const _CustomAppBarDesktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Row(
+        children: [
+          Image.asset(Assets.netflixLogo1),
+          const SizedBox(width: 12.0),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _AppBarButton(
+                  title: 'Home',
+                  onTap: () => print('Home'),
+                ),
+                _AppBarButton(
+                  title: 'TV Shows',
+                  onTap: () => print('TV Shows'),
+                ),
+                _AppBarButton(
+                  title: 'Movies',
+                  onTap: () => print('Movies'),
+                ),
+                _AppBarButton(
+                  title: 'Latest',
+                  onTap: () => print('Latest'),
+                ),
+                _AppBarButton(
+                  title: 'My List',
+                  onTap: () => print('My List'),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () => print('Search'),
+                  icon: const Icon(Icons.search),
+                  iconSize: 28.0,
+                  color: Colors.white,
+                ),
+                _AppBarButton(
+                  title: 'KIDS',
+                  onTap: () => print('KIDS'),
+                ),
+                _AppBarButton(
+                  title: 'DVD',
+                  onTap: () => print('DVD'),
+                ),
+                IconButton(
+                  onPressed: () => print('Gift'),
+                  icon: const Icon(Icons.card_giftcard),
+                  iconSize: 28.0,
+                  color: Colors.white,
+                ),
+                IconButton(
+                  onPressed: () => print('Notifications'),
+                  icon: const Icon(Icons.notifications),
+                  iconSize: 28.0,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AppBarButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+
+  const _AppBarButton({Key? key, required this.title, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
